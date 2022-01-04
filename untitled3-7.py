9de9d791da3a3ec0b65e79eef054642e93acdcaa
Beyzabay <93313816+Beyzabay@users.noreply.github.com>
2022-01-04 17:15:43 +0300

    Add files via upload

---
+++ untitled3-7.py
@@ -0,0 +1,21 @@
+def kitap(*kitabınismi,yazar):
+    print(kitabınismi,yazar, "tarafından yazılmıştır" )
+    
+kitap("İnce Memed " , "Teneke" , "Orta Direk" , "Yer DEmir Gök Bakır" , yazar = "Yaşar Kemal" )
+
+
+    
+
+    
+    
+    
+    
+
+    
+    
+
+
+
+
+    
+    
